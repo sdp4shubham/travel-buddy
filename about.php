275c474aff0450b954c5dfea6d63348acc9f88f4
sdp4shubham <165250869+sdp4shubham@users.noreply.github.com>
2024-03-28 12:10:00 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+   <h1>about us</h1>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="image">
+      <img src="images/about-img.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>why choose us?</h3>
+      <p>Choose our travel website for your next adventure because we offer a seamless booking experience with a user-friendly interface, competitive pricing, and a wide range of options. With our reputation for reliability and trustworthiness, personalized recommendations, and responsive customer support, we ensure that your travel planning process is stress-free and enjoyable. Benefit from comprehensive destination information, genuine customer reviews, and exclusive offers, all accessible on both desktop and mobile devices. Join our community of satisfied travelers and let us help you create unforgettable memories on your next journey.</p>
+     
+      <div class="icons-container">
+         <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>top destinations</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>affordable price</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 guide service</span>
+         </div>
+      </div>
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+   <h1 class="heading-title"> clients reviews </h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p> The website's user-friendly interface, competitive pricing, and exclusive deals made planning a breeze. The comprehensive destination information and genuine customer reviews helped me make confident decisions. The responsive customer support team was also a huge plus. Thanks to TRAVELBUDDY , my vacation exceeded expectations. Highly recommended!</p>
+            <h3>Mehrshad Nava</h3>
+            <span>traveler</span>
+            <img src="images/pic-1.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p> The website's user-friendly interface, competitive pricing, and exclusive deals made planning a breeze. The comprehensive destination information and genuine customer reviews helped me make confident decisions. The responsive customer support team was also a huge plus. Thanks to TRAVELBUDDY , my vacation exceeded expectations. Highly recommended!</p>
+            <h3>Aarushi savla</h3>
+            <span>traveler</span>
+            <img src="images/pic-2.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p> The website's user-friendly interface, competitive pricing, and exclusive deals made planning a breeze. The comprehensive destination information and genuine customer reviews helped me make confident decisions. The responsive customer support team was also a huge plus. Thanks to TRAVELBUDDY Travel, my vacation exceeded expectations. Highly recommended!</p>
+            <h3>Shubham Panchal</h3>
+            <span>traveler</span>
+            <img src="images/pic-3.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p> The website's user-friendly interface, competitive pricing, and exclusive deals made planning a breeze. The comprehensive destination information and genuine customer reviews helped me make confident decisions. The responsive customer support team was also a huge plus. Thanks to TRAVELBUDDY , my vacation exceeded expectations. Highly recommended!</p>
+            <h3>Mehrshad Nava</h3>
+            <span>traveler</span>
+            <img src="images/pic-4.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p> The website's user-friendly interface, competitive pricing, and exclusive deals made planning a breeze. The comprehensive destination information and genuine customer reviews helped me make confident decisions. The responsive customer support team was also a huge plus. Thanks to TRAVELBUDDY , my vacation exceeded expectations. Highly recommended!</p>
+            <h3>Aarushi Savla </h3>
+            <span>traveler</span>
+            <img src="images/pic-5.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p> The website's user-friendly interface, competitive pricing, and exclusive deals made planning a breeze. The comprehensive destination information and genuine customer reviews helped me make confident decisions. The responsive customer support team was also a huge plus. Thanks to TRAVELBUDDY Travel, my vacation exceeded expectations. Highly recommended!</p>
+            <h3>Shubham Panchal</h3>
+            <span>traveler</span>
+            <img src="images/pic-6.png" alt="">
+         </div>
+
+      </div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +91 8879-1829-30 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +91 9782-9920-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> travelbuddy@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400079 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>Aarushi Savla, Mehrshad Nava, Shubham Panchal</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
